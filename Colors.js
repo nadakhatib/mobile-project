6b8a830275c598a584652d8495857bb26b9bcee0
nada khatib <134955117+nadakhatib@users.noreply.github.com>
2025-06-11 16:12:09 +0300

    Create Colors.js

---
+++ Colors.js
@@ -0,0 +1,11 @@
+export default {
+  primary: '#4CAF50',
+  secondary: '#FFC107',
+  background: '#fff',
+  textPrimary: '#333',
+  textSecondary: '#666',
+  buttonText: '#fff',
+  inputBackground: '#f2f2f2',
+  borderColor: '#ddd',
+  error: '#f44336',
+};
